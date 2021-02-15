6c96f77126d7dc86c7915fd22bbb635366bcc1a8
bcongressi <bcongressi@ufl.edu>
2021-02-15 16:43:30 -0500

    Started Home Page / Navbar
    Added Footer and Header
    Updated Home Screen
    Added style file from bootswatch
    Added folder for components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
+
+//className py-3 for padding buffer
 const App =() => {
   return (
     <>
-      <h1>Welcome to Daydreamer</h1>
+    <Header></Header>
+    <main className='py-3'>
+      <Container>
+        <h1>Welcome to Daydreamer</h1>
+      </Container>
+    </main>
+    <Footer></Footer>
     </>
   );
 }
