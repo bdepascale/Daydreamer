971e17235534e3d7f9e4c2b228723fa73e6b25e5
bcongressi <bcongressi@ufl.edu>
2021-02-26 15:57:51 -0500

    Updated Homepage
    Homepage updated to include cards
    Products added
    images added
    descriptions added
    rating added

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Socks',
+    image: '/images/bikeSocks.jpg',
+    description:
+      'Stylish socks using recycled fabric with a fun and unique design',
+    brand: 'DayDreamer',
+    category: 'Clothing',
+    price: 10.99,
+    countInStock: 6,
+    rating: 5.0,
+    numReviews: 1,
+  },
+  {
+    _id: '2',
+    name: 'Jeans',
+    image: '/images/blueJeans.jpg',
+    description:
+      'Stylish jeans using recycled fabric with designer tears & highlights',
+    brand: 'DayDreamer',
+    category: 'Clothing',
+    price: 75.00,
+    countInStock: 5,
+    rating: 4.0,
+    numReviews: 2,
+  },
+  {
+    _id: '3',
+    name: 'Long Sleeve Shirt',
+    image: '/images/blueShirt.jpg',
+    description:
+      'Stylish long sleeve shirt using recycled fabric with skin fit',
+    brand: 'DayDreamer',
+    category: 'Clothing',
+    price: 29.99,
+    countInStock: 4,
+    rating: 3.0,
+    numReviews: 3,
+  },
+  {
+    _id: '4',
+    name: 'Skirt',
+    image: '/images/blueSkirt.jpg',
+    description:
+      'Stylish blue skirt using recycled fabric with soft material for skin',
+    brand: 'DayDreamer',
+    category: 'Clothing',
+    price: 44.99,
+    countInStock: 3,
+    rating: 2.0,
+    numReviews: 4,
+  },
+  {
+    _id: '5',
+    name: 'Rose Hat',
+    image: '/images/roseHat.jpg',
+    description:
+      'Stylish hat using recycled fabric with a beautiful rose on the front',
+    brand: 'DayDreamer',
+    category: 'Clothing',
+    price: 15.99,
+    countInStock: 2,
+    rating: 1.0,
+    numReviews: 5,
+  },
+  {
+    _id: '6',
+    name: 'Swim Trunks',
+    image: '/images/swimShorts.jpg',
+    description:
+      'Stylish swimwear using recycled fabric with special designer patterns',
+    brand: 'DayDreamer',
+    category: 'Clothing',
+    price: 29.99,
+    countInStock: 0,
+    rating: 3.5,
+    numReviews: 6,
+  },
+]
+
+export default products
