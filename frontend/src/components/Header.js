f56f33266a8337cb28d3eafc05b49c04d853ba54
Brian DePascale <bdepascale@gmail.com>
2021-02-27 11:01:09 -0500

    Added mission and contact buttons, updated color

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 
-
 /*  Navbar including login and shopping cart
     Navbar from https://react-bootstrap.github.io/components/navbar/ */
-const Header =() => {
-    return(
-        <header>
-            
-            <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
-                <Container>
-                    <Navbar.Brand href="/">Daydreamer</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ml-auto">
-                            <Nav.Link href="/shoppingCart"><i className='fas fa-shopping-cart'></i> Shopping Cart</Nav.Link>
-                            <Nav.Link href="/login"><i className='fas fa-user'></i> Sign In / Register</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </header>
-    )
-}
-export default Header
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='success' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>Daydreamer</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <Nav.Link href='/shoppingCart'>
+                <i className='fas fa-shopping-cart'></i> Shopping Cart
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i className='fas fa-user'></i> Sign In / Register
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i className='fas fa-contact'></i> Contact Us
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i className='fas fa-mission'></i> Our Mission
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+export default Header;
