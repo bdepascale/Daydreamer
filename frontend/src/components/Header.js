4a4716bab1c4089d37149fcc3b4354ccc4ca1f09
QuinteroSantiago <santiagoquintero@ufl.edu>
2021-04-16 14:07:04 -0400

    audio cue, search bar, merge conflicts

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import {LinkContainer} from 'react-router-bootstrap'
 import {Container, Navbar, Nav, NavDropdown} from 'react-bootstrap'
 import {useDispatch, useSelector} from 'react-redux'
 import {logout} from '../actions/userActions'
+import SearchBar from './SearchBar'
 
 /*  Navbar including login and shopping cart
     Navbar from https://react-bootstrap.github.io/components/navbar/ */
@@ -35,6 +37,7 @@ const Header = () => {
           
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
+          <Route render={({ history }) => <SearchBar history={history} />} />  
             <Nav className='ml-auto'>
               <LinkContainer to='/shoppingCart'>
                 <Nav.Link>
